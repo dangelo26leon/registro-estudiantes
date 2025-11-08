a2abf3aa5cc8c0b41510a7647b0f54c15527f5e9
dangelo26leon <dangeloleon75@gmail.com>
2025-11-07 20:43:22 -0500

    ci: add Discord notificación on failure

--- src/test/java/com/estudiantes/AppTest.java
+++ src/test/java/com/estudiantes/AppTest.java
@@ -1,9 +1,21 @@
 package com.estudiantes;
+
 import org.junit.Test;
 import static org.junit.Assert.*;
+
 public class AppTest {
+
     @Test
-    public void testApp() {
+    public void testAppRuns() {
+        App.main(new String[]{});
         assertTrue(true);
+    }
+
+    @Test
+    public void testSum() {
+        int a = 2;
+        int b = 3;
+        int resultado = a + b;
+        assertEquals(5, resultado);
     }
 }
